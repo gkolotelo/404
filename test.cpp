9f62087e00ad3bee7781be7c60f56c906a7a5dfb
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-09 14:18:01 -0300

    Added test files

---
+++ test.cpp
@@ -0,0 +1,11 @@
+/* Lexycal Analysis                    */
+/* Author: Guilherme Kairalla Kolotelo */
+
+#include <stdio.h>
+#include <string.h>
+#include "reading.cpp"
+
+int main(int argc, char *argv[])
+{
+	
+}
