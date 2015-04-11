a356000bddf4240ab3080c2df0a744ddf8f7fa8b
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-10 22:00:34 -0300

    stream testing

--- test_gkk.cpp
+++ test_gkk.cpp
@@ -2,10 +2,39 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "reading.cpp"
+#include <mach/mach_time.h>
+
+uint64_t        start_, end_, elapsed;
+char data[10001];
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-	//test
-	
+	//if(argc != 2)
+    //{
+    //    printf("%s", "File path needed\n");
+    //    return 1;
+    //}
+
+    start_ = mach_absolute_time();
+
+    //std::fstream fs;
+    //fs.open(argv[1], std::fstream::in);
+
+    cin >> data;
+
+	end_ = mach_absolute_time();
+	elapsed = end_ - start_;
+
+	//fs.read(data, 10000);
+
+	cout << elapsed << "µs" << endl;
+
+
+
+
+	cout << "Output: " << data << endl;
+
+
 }
