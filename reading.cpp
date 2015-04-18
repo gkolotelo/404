551b14bb2f3d2a6dad50fee7a201c3abfd3b5ac2
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-18 16:31:09 -0300

    changed methods to use TokenContainer type

--- reading.cpp
+++ reading.cpp
@@ -31,7 +31,7 @@ ElementType getType(char *in)
 	return Instruction;
 }
 
-void tokenize(char *in, string *tokens)
+void tokenize(char *in, TokenContainer *TC)
 {
 	int i = 0;
 	stringstream data;
@@ -41,16 +41,80 @@ void tokenize(char *in, string *tokens)
 
 	while(token != NULL)
 	{
-		(tokens[++i]).assign(token);
+		(TC->tokens[i++]).assign(token);
 		//cout << token << '|';
 		token = strtok(NULL, ", \t");
 	}
-	tokens[0] = i; // Store number of tokens on zero index
-	//cout << stoi(tokens[0] + 48) << '|';
-	//for(i=1; i < 5; i++)
+	TC->amount = i; // Store number of tokens on zero index
+	//cout << TC->amount << '|';
+	//for(i=0; i < 5; i++)
 	//{
-	//	cout << tokens[i] << '|';
+	//	cout << TC->tokens[i] << '|';
 	//}
 	//cout << endl;
-	//cout << endl;
 }
+
+Element getElement(TokenContainer TC)
+{
+	int i = 0;
+
+}
+
+
+//string *tokenize(char *in)
+//{
+//	string tokens[5];
+//	int i = 0;
+//	stringstream data;
+//	char *label, *directive, *instruction, *aux;
+//
+//	label = strtok(in, ":");
+//	remaining = strtok(NULL, ".");
+//	if(remaining == NULL) //INVALID!! Label is only element of line
+//	{
+//
+//
+//	}
+//	dir_or_int = strtok(remaining, ".");
+//	remaining = strtok(NULL, ".");
+//	if(remaining == NULL) // Instruction is only remaining element of line
+//	{
+//		// Parse instruction:
+//
+//	}
+//	// Parse directive:
+//	cout << (token == NULL) << endl;
+//
+//	while(token != NULL)
+//	{
+//		data << token << '|';
+//
+//		//if(getType(token) == label)
+//		//{
+////
+//		//}
+//		//else if
+//
+//		token = strtok(NULL, "5");
+//	}
+//	cout << data.rdbuf() << endl;
+//}
+
+//string *tokenize_old(char *in)
+//{
+//	string tokens[5];
+//	int i = 0;
+//	stringstream data;
+//	char *token;
+//
+//	//cout << "|" << strtok(in, "(") << "|" << endl;
+//
+//	token = strtok(in, ", \t");
+//
+//	while(token != NULL)
+//	{
+//		cout << token << '|';
+//		token = strtok(NULL, ", \t");
+//	}
+//	cout << token << endl;
+//}
