c854bfb0257932ad98d3c61bb6ef8ce859036245
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-18 15:50:16 -0300

    moved type definitions to types.cpp

---
+++ types.cpp
@@ -0,0 +1,75 @@
+// types.cpp
+//
+//
+//
+//
+
+
+//Tipos de elementos
+typedef enum{
+	Label,
+	Instruction,
+	Directive
+}ElementType;
+
+
+//Tipos de operacao
+typedef enum{
+	Load_MQ,
+	Load_MQ_MX,
+	Stor_MX,
+	Load_MX,
+	Load_MX_neg,
+	Load_MX_mod,
+	Jump_M_L,
+	Jump_M_R,
+	Jump_M_cond_L,
+	Jump_M_cond_R,
+	Add_MX,
+	Add_MX_mod,
+	Sub_MX,
+	Sub_MX_mod,
+	Mul_MX,
+	Div_MX,
+	Lsh,
+	Rsh,
+	Stor_M_L,
+	Stor_M_R
+}OpCodeType;
+
+//Tipos de diretivas
+typedef enum{
+	Org,
+	Align,
+	Wfill,
+	Word,
+	Set
+}DirectiveType;
+
+//Elemento
+class Element{
+	ElementType type;
+	string content;
+	//Algo mais aqui?
+};
+
+//Construtor para elemento do tipo Label
+Element::Element(string _content){
+	//Campos de Label
+	type = Label;
+	content = _content;
+}
+
+//Construtor para elemento do tipo Diretiva
+Element::Element(ElementType _element, DirectiveType _dir){
+	type = Directive;
+	Directive dir = _dir;
+	content = _content;
+}
+
+//Construtor para elemento tipo Operacao
+Element::Element(ElementType _element, OpCodeType _op){
+	type = Instruction;
+	opcode = _op;
+	content = _content;
+}
