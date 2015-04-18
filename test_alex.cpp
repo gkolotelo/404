1b4a1b276bf2f97f5641db8e35314d2f55ddb323
alexseidy <alexseidy@Alexandres-MacBook-Air-2.local>
2015-04-17 21:12:06 -0300

    updates in test_alex.cpp

--- test_alex.cpp
+++ test_alex.cpp
@@ -2,10 +2,60 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "reading.cpp"
 
-int main(int argc, char *argv[])
-{
-	//test
+
+//Tipos de elementos
+typedef enum ElementType{
+	label,
+	instruction,
+	directive
+}ElementType;
+
+//Tipos de operacao
+typedef enum OpCodeType{
+	
+}OpCodeType;
+
+//Tipos de diretivas
+typedef enum DirectiveType{
+	
+}DirectiveType;
+
+//Elemento
+class Element{
+	ElementType type;
+	string content;
+	
+};
+
+Element::Element(ElementType _element){
+	type = label;
+}
+
+Element::Element(ElementType _element, DirectiveType _dir){
+	Directive dir
+	sdadsa
+
+}
+
+Element::Element(ElementType _element, OpCodeType _op){
 	
 }
+
+
+int main(){
+	Element elem();
+	cout << ;
+}
+
+//int interpreter();
+
+//void assembly(){
+//	//Loop de leitura
+//	while(){
+//		//Loop de traducao
+//		while(){
+//
+//		}
+//	}
+//}
