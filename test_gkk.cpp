9efea5c5ccd6c676fc4091145f8de1e76c9f837c
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-18 15:49:48 -0300

    finished tokenize

--- test_gkk.cpp
+++ test_gkk.cpp
@@ -3,26 +3,76 @@
 #include <fstream>
 #include <sstream>
 #include <mach/mach_time.h>
+#include "reading.cpp"
+#include "types.cpp"
 
 uint64_t        start_, end_, elapsed_;
 
 using namespace std;
 
-// Returns string up to comment (delimited by '#')
-string getLineNoComment(string in)
-{
-	string tempStr;
-	stringstream data;
-	data << in;
-	getline(data, tempStr, '#');
-	return tempStr;
-}
 
-list<string> tokenize(string in)
-{
 
-}
 
+
+
+
+//string *tokenize(char *in)
+//{
+//	string tokens[5];
+//	int i = 0;
+//	stringstream data;
+//	char *label, *directive, *instruction, *aux;
+//
+//	label = strtok(in, ":");
+//	remaining = strtok(NULL, ".");
+//	if(remaining == NULL) //INVALID!! Label is only element of line
+//	{
+//
+//
+//	}
+//	dir_or_int = strtok(remaining, ".");
+//	remaining = strtok(NULL, ".");
+//	if(remaining == NULL) // Instruction is only remaining element of line
+//	{
+//		// Parse instruction:
+//
+//	}
+//	// Parse directive:
+//	cout << (token == NULL) << endl;
+//
+//	while(token != NULL)
+//	{
+//		data << token << '|';
+//
+//		//if(getType(token) == label)
+//		//{
+////
+//		//}
+//		//else if
+//
+//		token = strtok(NULL, "5");
+//	}
+//	cout << data.rdbuf() << endl;
+//}
+
+//string *tokenize_old(char *in)
+//{
+//	string tokens[5];
+//	int i = 0;
+//	stringstream data;
+//	char *token;
+//
+//	//cout << "|" << strtok(in, "(") << "|" << endl;
+//
+//	token = strtok(in, ", \t");
+//
+//	while(token != NULL)
+//	{
+//		cout << token << '|';
+//		token = strtok(NULL, ", \t");
+//	}
+//	cout << token << endl;
+//}
 
 int main(int argc, char *argv[])
 {
@@ -34,8 +84,8 @@ int main(int argc, char *argv[])
 
 	fstream fs;
 	string tempStr;
+	string tokens[5];
 	char tempStrCA[100];
-	char *token;
 
 	start_ = mach_absolute_time();
 
@@ -43,20 +93,22 @@ int main(int argc, char *argv[])
 
 	while(getline(fs, tempStr, '\n'))
 	{
-		tempStr = getLineNoComment(tempStr); // Strip comments out of input line
+		tempStr = getLineNoCommentLowercase(tempStr); // Strip comments out of input line
 		if (tempStr.length() != 0)
 		{
+			//cout << tempStr << endl;
 			stringstream data;
 			//cout << tempStr << endl;
 			strcpy(tempStrCA, tempStr.c_str());
-			token = strtok(tempStrCA, " \t");
-			data << '|' << token << '|';
-			while(token != NULL)
-			{
-				token = strtok(NULL, " \t");
-				if(token != NULL) data << token << '|';
-			}
-			cout << data.rdbuf() << endl;
+			tokenize(tempStrCA, tokens);
+			//token = strtok(tempStrCA, " \t");
+			//data << '|' << token << '|';
+			//while(token != NULL)
+			//{
+			//	token = strtok(NULL, " \t");
+			//	if(token != NULL) data << token << '|';
+			//}
+			//cout << data.rdbuf() << endl;
 		}
 		
 		
