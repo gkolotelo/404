7d96f15a2144209b464af79df61eb06cdac92608
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-19 17:40:25 -0300

    Linting

--- errors.cpp
+++ errors.cpp
@@ -4,23 +4,19 @@
 //
 
 
-typedef enum{
-	Invalid_Label_Error,
-	Invalid_Direcive_Error,
-	Invalid_Instruction_Error,
-
-
+typedef enum {
+    Invalid_Label_Error,
+    Invalid_Direcive_Error,
+    Invalid_Instruction_Error
 }ErrorType;
 
-class Error
-{
-public:
-	error(ErrorType _error, );
-private:
-	ErrorType _err;
+class Error {
+ public:
+    error(ErrorType _error, );
+ private:
+    ErrorType _err;
 };
 
-Error::error(ErrorType _error, int in_line)
-{
+Error::error(ErrorType _error, int in_line) {
 
 }
