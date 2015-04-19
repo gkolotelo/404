bd316ef775bd202f4c402182dadfa3ecf012fefe
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-19 17:00:47 -0300

    Added errors.cpp library

---
+++ errors.cpp
@@ -0,0 +1,26 @@
+//
+// errors.cpp
+//
+//
+
+
+typedef enum{
+	Invalid_Label_Error,
+	Invalid_Direcive_Error,
+	Invalid_Instruction_Error,
+
+
+}ErrorType;
+
+class Error
+{
+public:
+	error(ErrorType _error, );
+private:
+	ErrorType _err;
+};
+
+Error::error(ErrorType _error, int in_line)
+{
+
+}
