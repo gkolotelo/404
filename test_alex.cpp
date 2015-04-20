81f1bd177272e6c24a88ddc107d0abb6f16da036
alexseidy <alexseidy@Alexandres-MacBook-Air-2.local>
2015-04-19 22:41:55 -0300

    update in test_alex.cpp

--- test_alex.cpp
+++ test_alex.cpp
@@ -3,7 +3,10 @@
 #include <fstream>
 #include <sstream>
 #include "types.cpp"
-//#include "reading.cpp"
+#include "reading.cpp"
+#include <list>
+
+using namespace std;
 
 //Main para testes
 //int main(){
@@ -22,45 +25,119 @@ typedef enum{
 }Side;
 
 class MemoryMap{
-	Element elem;
-	int addr;
-	Side side;
-	tipo label_link;
+    Element* elem;
+    int addr;
+    Side side;
 
 };
 
-//Tem que pensar como vai ser isso
-//Acho q ao inves de ser uma funcao sozinha, 
-//vai ser uma etapa da main ou de outra funcao.
-void add(){
-	switch(elem.type){
-		case Label:
-		case Instruction:
-		case Directive:
-	}
+
+
+MemoryMap::MemoryMap(Element* _e){
+    elem = _e;
+    addr = 0;
+    //Outras possiveis propriedades
 }
 
 
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        printf("%s", "File path needed\n");
+        return 1;
+    }
+
+    fstream fs;
+    string tempStr, tempStr2;
+
+    fs.open(argv[1], fstream::in);
+
+    std::list<MemoryMap> memoryMap;
+    std::list<MemoryMap>::iterator mIterator;
+    Element* elem;
+
+    //assembly();
+    //Para cada linha no arquivo
+    //Comeca a montar o MemoryMap, elemento a elemento
+    while(getline(fs, tempStr, '\n'))
+    {
+        tempStr = getLineNoCommentLowercase(tempStr); // Strip comments out of input line
+
+        /* * * * * * * * * * * * *
+         * Tratamento de labels  *
+         * * * * * * * * * * * * */
+
+        //Procura por label
+        elem = getElement(tokenize(splitLabel(&tempStr)));
+        
+        if(elem != NULL){
+            //Definicao de uma label
+            //Insere no container de labels (labellink?)
+            //TODO: chamar a insercao de label
+        }
+        //else //Nao tem label
 
 
-//void montagem(){
-//	while(leitura(arquivo, linha)){
-//		for each elemento in linha{ //acho q precisa de um interpretador
-//			switch(elemento.type){
-//				case label{
-//
-//				}
-//				case directive{
-//
-//				}
-//				case operation{
-//
-//				}
-//			}
-//		}
-//	}
-//}
-//
-//void escrita(memorymap){
-//
-//}
+        /* * * * * * * * * * * * * * * * * * * *
+         * Tratamento de diretiva ou instrucao *
+         * * * * * * * * * * * * * * * * * * * */
+
+        //Procura por diretiva ou instrucao
+        elem = getElement(tokenize(tempStr));
+
+        if((*elem).type == Instruction){
+            //Instrucao
+            switch((*elem).opcode){
+                //Instrucoes sem endereco
+                case Load_MQ:
+                case Lsh:
+                case Rsh:
+                    memoryMap.push_back(MemoryMap(elem));
+                    //TODO: controle de endereco e lado
+                    break;
+
+                //Instrucoes com endereco
+                case Load_MQ_MX:
+                case Stor_MX:
+                case Load_MX:
+                case Load_MX_neg:
+                case Load_MX_abs:
+                case Jump_M:
+                case Jump_M_P:
+                case Add_MX:
+                case Add_MX_abs:
+                case Sub_MX:
+                case Sub_MX_abs:
+                case Mul_MX:
+                case Div_MX:
+                case Stor_M:
+                    //Resolve os labels
+                    //TODO: chamar o "resolvedor de label"
+                    break;
+            }
+        }
+        else{
+            //Diretiva
+            //TODO: switch-cases
+            switch((*elem).DirectiveType){
+                case Org:
+                    //Mudar origem
+                    break;
+                case Align:
+                    //Pular linha
+                    break;
+                case Wfill:
+                    //Preencher varias linhas com a mesma palavra
+                    break;
+                case Word:
+                    //Preencher uma linha com uma palavra
+                    break;
+                case Set:
+                    //Parecido com label para uma word??
+                    //TODO: verificar a semelhanca e possivel juncao
+                    break;
+            }
+        }
+    }
+}
