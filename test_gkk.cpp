7ee573bc06cfc2b5d89e91a124731a400c8cc597
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-20 02:53:18 -0300

    Messing around with printing of memorymap

--- test_gkk.cpp
+++ test_gkk.cpp
@@ -16,6 +16,55 @@ uint64_t        start_, end_, elapsed_;
 
 using namespace std;
 
+
+
+
+typedef enum{
+    Left,
+    Right
+}Side;
+
+typedef struct {
+    Element *elem;
+    int addr;
+    Side side;
+} MemoryElement;
+
+class MemoryMap{
+ private:
+    list<MemoryElement> memoryList;
+    list<MemoryElement>::iterator memoryListIterator;
+    int cursor;
+
+    string computeAddress(int addr) {
+
+    }
+
+    string generateLine(string addr, MemoryElement el1, MemoryElement el2) {
+            
+    }
+
+ public:
+    void add(Element el);
+    void printMemoryMap();
+    //TODO: mais algum metodo?
+};
+
+void MemoryMap::printMemoryMap() {
+    MemoryElement e1, e2;
+    string line;
+    for (memoryListIterator = memoryList.begin(); memoryListIterator != memoryList.end(); memoryListIterator++)
+    {
+        e1 = *memoryListIterator++;
+        e2 = *memoryListIterator++;  // Don't need to check if it's past end, because elements must always come in pairs
+
+        line = generateLine(computeAddress(e1.addr), e1, e2);
+    }
+}
+
+
+
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("%s", "File path needed\n");
@@ -37,10 +86,8 @@ int main(int argc, char *argv[]) {
         // Strip comments out of input line
         tempStr = getLineNoCommentLowercase(tempStr);
         // Cria Element somente dos tokens correspondentes ao label, se existir
-        //cout << "tempStr: " << tempStr << endl;
         getElement(tokenize(splitLabel(&tempStr)));
         // Cria Element de diretiva ou instrução
-        //cout << "tempStr: " << tempStr << endl;
         getElement(tokenize(tempStr));
     
         // cout << tempStr << endl;
@@ -57,37 +104,43 @@ int main(int argc, char *argv[]) {
         // }
     }
 
-    DirectiveContentContainer CC;
-    string input = "51435,";
-    CC = contentParser(input);
-    cout << input << ' ' << CC.content1 << ' ' << CC.content2 << ' ' << CC.amount << endl;
 
 
 
-    //List Example:
-    // #include <list>
-    std::list<string> list_sample;
-    std::list<string>::iterator list_iterator;
-    for (int i = 0; i < 10; i++) {  // Adicionar 10 inteiros a lista
-        list_sample.push_back( ("Number: " + to_string(i)) ); // Adiciona ao fim da lista, tem outras funcoes de insercao tbm
-        cout << "Just inserted: '" << list_sample.back() << "' to list_sample" << endl;  // .back acessa ultimo elemento da lista sem remove-lo 
-    }
-    cout << "# of elements in list: " << list_sample.size() << endl;
 
-    list_iterator = list_sample.begin(); // Iterator no comeco da lista
-    advance(list_iterator, 5); // avanca iterator em 5 elementos
-    cout << *list_iterator << endl; // imprime elemento na posicao 6 do iterator ("Number: 5")
-    list_iterator = list_sample.erase(list_iterator); // Apaga elemento na posicao do iterator e retorna novo iterator: atencao vc deve pegar o iterator novo pq o antigo nao funciona mais, o elemento foi removido
-    list_iterator = list_sample.insert(list_iterator, "Hello World!!!"); // insere elemento na posicao do iterator, e retorna o iterator para o elemento que acabou de ser inserido 
-    cout << *list_iterator << endl; // imprime novo elemento na posicao do iterator
 
-    cout << "Printing list: \n" << endl;
-    for (list_iterator = list_sample.begin(); list_iterator != list_sample.end(); list_iterator++) {  // Iterar pelo numero de elementos
-        string tmp = *list_iterator;  // pegar elementos do comeco da lista -> FIFO
-        list_sample.pop_front();  // remover elemento
-        cout << tmp << endl;  // imprimir elemento
-    }
-    cout << "# of elements in list: " << list_sample.size() << endl;
+
+    // DirectiveContentContainer CC;
+    // string input = "-0x1010";
+    // CC = contentParser(input, "");
+    // cout << input << ' ' << CC.content1 << ' ' << CC.content2 << ' ' << CC.amount << endl;
+
+
+
+    // //List Example:
+    // // #include <list>
+    // std::list<string> list_sample;
+    // std::list<string>::iterator list_iterator;
+    // for (int i = 0; i < 10; i++) {  // Adicionar 10 inteiros a lista
+    //     list_sample.push_back( ("Number: " + to_string(i)) ); // Adiciona ao fim da lista, tem outras funcoes de insercao tbm
+    //     cout << "Just inserted: '" << list_sample.back() << "' to list_sample" << endl;  // .back acessa ultimo elemento da lista sem remove-lo 
+    // }
+    // cout << "# of elements in list: " << list_sample.size() << endl;
+    // 
+    // list_iterator = list_sample.begin(); // Iterator no comeco da lista
+    // advance(list_iterator, 5); // avanca iterator em 5 elementos
+    // cout << *list_iterator << endl; // imprime elemento na posicao 6 do iterator ("Number: 5")
+    // list_iterator = list_sample.erase(list_iterator); // Apaga elemento na posicao do iterator e retorna novo iterator: atencao vc deve pegar o iterator novo pq o antigo nao funciona mais, o elemento foi removido
+    // list_iterator = list_sample.insert(list_iterator, "Hello World!!!"); // insere elemento na posicao do iterator, e retorna o iterator para o elemento que acabou de ser inserido 
+    // cout << *list_iterator << endl; // imprime novo elemento na posicao do iterator
+    // 
+    // cout << "Printing list: \n" << endl;
+    // for (list_iterator = list_sample.begin(); list_iterator != list_sample.end(); list_iterator++) {  // Iterar pelo numero de elementos
+    //     string tmp = *list_iterator;  // pegar elementos do comeco da lista -> FIFO
+    //     list_sample.pop_front();  // remover elemento
+    //     cout << tmp << endl;  // imprimir elemento
+    // }
+    // cout << "# of elements in list: " << list_sample.size() << endl;
 
 
     end_ = mach_absolute_time();
