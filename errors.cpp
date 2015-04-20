8a110822e55a37a862c7715d336d204401d92638
Guilherme Kairalla Kolotelo <gkolotelo@gmail.com>
2015-04-20 16:56:14 -0300

    Minor changes

--- errors.cpp
+++ errors.cpp
@@ -8,6 +8,8 @@ typedef enum {
     Invalid_Label_Error,
     Invalid_Direcive_Error,
     Invalid_Instruction_Error
+    //Labels and sets must not start with nubers
+    //more than one label per line
 }ErrorType;
 
 class Error {
