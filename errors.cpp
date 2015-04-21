153a637dbe902739f479e59a0ad5293f25a9659c
alexseidy <alexseidy@Alexandres-MacBook-Air-2.local>
2015-04-21 16:50:43 -0300

    update in errors.cpp

--- errors.cpp
+++ errors.cpp
@@ -7,7 +7,9 @@
 typedef enum {
     Invalid_Label_Error,
     Invalid_Direcive_Error,
-    Invalid_Instruction_Error
+    Invalid_Instruction_Error,
+    Label_Set_Not_Defined,
+    Label_Set_Duplicate
     //Labels and sets must not start with nubers
     //more than one label per line
 }ErrorType;
@@ -27,11 +29,29 @@ class Error {
 
 
 void Error::callError(ErrorType _error, int in_line) {
-	string errorMessage;
-	errorMessage = "Error: "
-	strcat(errorMessage)
-	switch(_error){
-		default:
-	}
+	char errorMessage[255];
+	if(in_line != -1)
+		sprintf(errorMessage, "Error: Line %d: ", in_line)
+	else
+		sprintf(errorMessage, "Error: ")
 
+	switch(_error){
+		case Invalid_Label_Error:
+			strcat(errorMessage, "");
+			break;
+		case Invalid_Direcive_Error:
+			strcat(errorMessage, "");
+			break;
+		case Invalid_Instruction_Error:
+			strcat(errorMessage, "");
+			break;
+		case Label_Set_Not_Defined:
+			strcat(errorMessage, "Label or set name not defined.");
+			break;
+		case Label_Set_Duplicate:
+			strcat(errorMessage, "Label or set name duplicate.");
+			break;
+	}
+	printf("%s\n", errorMessage);
+	exit(EXIT_FAILURE);
 }
